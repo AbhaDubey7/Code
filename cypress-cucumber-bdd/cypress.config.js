d696372da93f4a90320baba58fa8c2bbe4cb9d4a
AbhaDubey7 <166500415+AbhaDubey7@users.noreply.github.com>
2024-07-31 22:17:31 +1000

    Initial commit

--- cypress-cucumber-bdd/cypress.config.js
+++ cypress-cucumber-bdd/cypress.config.js
@@ -1,12 +1,4 @@
-// const { defineConfig } = require("cypress");
 
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
 
 const { defineConfig } = require('cypress');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
